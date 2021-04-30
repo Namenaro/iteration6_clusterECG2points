a2aa58c62046d1ef17bfd8e902ea5bad9ce85e18
yana <nanenaro@gmail.com>
2021-04-30 22:49:51 +0300

    gfhfhg

--- peripheria.py
+++ peripheria.py
@@ -17,3 +17,33 @@ plt.plot(y6, ':', label='filtered, sigma=6')
 plt.legend()
 plt.grid()
 plt.show()
+
+
+################################
+a = np.array([1,3,9,3,7,1])
+max_index = np.argmax(a)
+print (max_index)
+################################
+
+from scipy.interpolate import interp1d
+from scipy import signal
+
+def downsample(array, npts):
+    interpolated = interp1d(np.arange(len(array)), array, axis = 0, fill_value = 'extrapolate')
+    downsampled = interpolated(np.linspace(0, len(array), npts))
+    return downsampled
+
+y = np.array([1,2,3,4,5,6,10])
+#downsampled_y = signal.resample(y, 4)
+downsampled_y = downsample(y, 4)
+print(downsampled_y)
+
+################################
+import torch
+from torch import nn
+m = nn.AvgPool1d(3, stride=2)
+y = np.array([1,2,3,4,5,6,10])
+
+y= m(torch.tensor([[y]])).numpy()
+print (np.squeeze(y))
+####################################
